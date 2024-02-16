370de30453c250662564bb2b3b9d10025e7607a6
= <Robinheqd@gmail.com>
2024-02-16 18:12:53 +0000

    update web server

--- main.py
+++ main.py
@@ -91,6 +91,13 @@ Member_Add_Funds_Amount = ""
 Member_Add_Funds_Success = False
 Member_Add_Funds_Result = ""
 
+Join_Auction = False
+Join_Auction_Name = ""
+Join_Auction_Title = ""
+
+Join_Auction_Success = False
+Join_Auction_Error = False
+
 @app.route('/')
 def index():
 	return "Nothing to see here"
@@ -99,7 +106,7 @@ def index():
 def server():
 	while True:
 		time.sleep(0.001)
-		global New_Group, groupUserEmail, groupName, groupDescription, groupMaxFunds, New_Auction_User, auctionMemberName, auctionMemberEmail, New_Auction_Host, auctionHostEmail, auctionHostName, New_Auction, auctionTitle, auctionDescription, auctionStartPrice, auctionLength, New_Login_Host, Login_Host_Email, New_Login_Member, Login_Member_Email, Host_Get_Auctions, Host_Get_Auctions_Email, Host_Auction_Manage, Host_Auction_Manage_Email, Host_Auction_Manage_Title, Member_Get_Groups, Member_Get_Groups_Email, Member_Join_Group, Member_Join_Group_Email, Member_Join_Group_Name, Group_Data, Group_Data_Name, Group_Data_Email, Member_Add_Funds, Member_Add_Funds_Email, Member_Add_Funds_Name, Member_Add_Funds_Amount
+		global New_Group, groupUserEmail, groupName, groupDescription, groupMaxFunds, New_Auction_User, auctionMemberName, auctionMemberEmail, New_Auction_Host, auctionHostEmail, auctionHostName, New_Auction, auctionTitle, auctionDescription, auctionStartPrice, auctionLength, New_Login_Host, Login_Host_Email, New_Login_Member, Login_Member_Email, Host_Get_Auctions, Host_Get_Auctions_Email, Host_Auction_Manage, Host_Auction_Manage_Email, Host_Auction_Manage_Title, Member_Get_Groups, Member_Get_Groups_Email, Member_Join_Group, Member_Join_Group_Email, Member_Join_Group_Name, Group_Data, Group_Data_Name, Group_Data_Email, Member_Add_Funds, Member_Add_Funds_Email, Member_Add_Funds_Name, Member_Add_Funds_Amount, Join_Auction, Join_Auction_Name, Join_Auction_Title
 		if New_Group:
 			data = {
 			"action": "create-group",
@@ -198,6 +205,14 @@ def server():
 			}
 			Member_Add_Funds = False
 			return json.dumps(data)
+		elif Join_Auction:
+			data = {
+			"action": "join-auction",
+			"groupName": Join_Auction_Name,
+			"auctionTitle": Join_Auction_Title
+			}
+			Join_Auction = False
+			return json.dumps(data)
 		return "Nothing"
 
 @app.route("/create-group", methods=['POST'])
@@ -291,7 +306,7 @@ def auctionCheck():
 
 @app.route("/validate", methods=['POST'])
 def validate():
-	global Create_Host_Error, Create_Host_Success, Create_Auction_Error, Create_Auction_Success, Create_Member_Error, Create_Member_Success, Create_Group_Error, Create_Group_Success, Login_Host_Error, Login_Host_Success, Login_Host_Name, Login_Member_Error, Login_Member_Success, Login_Member_Name, Host_Get_Auctions_Result, Host_Get_Auctions_Result_Worked, Host_Get_Auction_Manage_Result, Host_Get_Auction_Manage_Worked, Member_Get_Group_Result, Member_Get_Group_Result_Worked, Member_Join_Group_Error, Member_Join_Group_Success, Group_Data_Result_Worked, Group_Data_Result, Member_Add_Funds_Success, Member_Add_Funds_Result
+	global Create_Host_Error, Create_Host_Success, Create_Auction_Error, Create_Auction_Success, Create_Member_Error, Create_Member_Success, Create_Group_Error, Create_Group_Success, Login_Host_Error, Login_Host_Success, Login_Host_Name, Login_Member_Error, Login_Member_Success, Login_Member_Name, Host_Get_Auctions_Result, Host_Get_Auctions_Result_Worked, Host_Get_Auction_Manage_Result, Host_Get_Auction_Manage_Worked, Member_Get_Group_Result, Member_Get_Group_Result_Worked, Member_Join_Group_Error, Member_Join_Group_Success, Group_Data_Result_Worked, Group_Data_Result, Member_Add_Funds_Success, Member_Add_Funds_Result, Join_Auction_Success, Join_Auction_Error
 	if json.loads(request.json)['Result'] == "Host-Created":
 		Create_Host_Success = True
 	elif json.loads(request.json)['Result'] == "Host-Exists":
@@ -342,6 +357,10 @@ def validate():
 	elif json.loads(request.json)['Result'] == "Funds-Added":
 		Member_Add_Funds_Success = True
 		Member_Add_Funds_Result = json.loads(request.json)['Funds']
+	elif json.loads(request.json)['Result'] == "Auction-Joined":
+		Join_Auction_Success = True
+	elif json.loads(request.json)['Result'] == "Auction-Not-Joined":
+		Join_Auction_Error = True
 	return "done"
 
 @app.route("/login-host", methods=['POST'])
@@ -480,6 +499,25 @@ def memberAddFundsResult():
 	if Member_Add_Funds_Success:
 		Member_Add_Funds_Success = False
 		return Member_Add_Funds_Result
+	return "Nothing"
+
+@app.route("/join-auction", methods=['POST'])
+def memberJoinAuction():
+	global Join_Auction, Join_Auction_Name, Join_Auction_Title
+	Join_Auction = True
+	Join_Auction_Name = json.loads(request.json)['groupName']
+	Join_Auction_Title = json.loads(request.json)['auctionTitle']
+	return "Done"
+
+@app.route("/check-join-auction")
+def memberCheckJoinAuction():
+	global Join_Auction_Success, Join_Auction_Error
+	if Join_Auction_Success:
+		Join_Auction_Success = False
+		return "Success"
+	elif Join_Auction_Error:
+		Join_Auction_Error = False
+		return "Error"
 	return "Nothing"
 
 if __name__ == "__main__":
